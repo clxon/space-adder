1af56a82f7c2df57f7333c12aacb5e54ff5814ce
Nicklaus Ning <Deeplemon@163.com>
2020-05-11 20:54:39 +0800

    Add .

---
+++ index.js
@@ -0,0 +1,20 @@
+﻿var addTheSpace = function () {
+    var input = $('#before').val()
+    var num = window.parseInt($('#num').val()) || 1
+    var map = Array.prototype.map
+    var arr = map.call(input, function (n) {
+        return n
+    })
+    console.log(arr)
+    input = ''
+    map.call(arr, function (n) {
+        var middle = ''
+        for (var i = 0; i < num; i++) {
+            middle += ' '
+        }
+        input += n + middle
+    })
+    $('#result').text(input)
+    console.log(input)
+    console.log(num)
+}
